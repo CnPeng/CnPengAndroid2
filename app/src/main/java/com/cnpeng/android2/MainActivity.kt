3d1c21cc27c7c3782234b7ec52b29cf8be9b9d0b
CnPeng <893612134@qq.com>
2018-08-16 17:09:54 +0800

    完善首页跳转事件

--- app/src/main/java/com/cnpeng/android2/MainActivity.kt
+++ app/src/main/java/com/cnpeng/android2/MainActivity.kt
@@ -1,30 +1,37 @@
 package com.cnpeng.android2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.cnpeng.android2.a01_chips.ChipActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private lateinit var mActivity: MainActivity
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        mActivity = this
 
-        // Example of a call to a native method
-        sample_text.text = stringFromJNI()
+        initClickListener()
     }
 
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
-    external fun stringFromJNI(): String
+    private fun initClickListener() {
+        tv_chip.setOnClickListener(this)
 
-    companion object {
+    }
 
-        // Used to load the 'native-lib' library on application startup.
-        init {
-            System.loadLibrary("native-lib")
+    override fun onClick(v: View?) {
+        val intent = Intent()
+        val viewID = v?.id
+
+        when (viewID) {
+            R.id.tv_chip -> intent.setClass(mActivity, ChipActivity::class.java)
+
         }
+        startActivity(intent)
     }
+
 }
