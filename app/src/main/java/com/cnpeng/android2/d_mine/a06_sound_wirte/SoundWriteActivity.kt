c04fa64ad6f6783d6606fbe32c7ad5ac4d2b195f
CnPeng <893612134@qq.com>
2019-01-08 18:19:43 +0800

    完善语音输入识别的逻辑

--- app/src/main/java/com/cnpeng/android2/d_mine/a06_sound_wirte/SoundWriteActivity.kt
+++ app/src/main/java/com/cnpeng/android2/d_mine/a06_sound_wirte/SoundWriteActivity.kt
@@ -1,13 +1,20 @@
 package com.cnpeng.android2.d_mine.a06_sound_wirte
 
+import android.Manifest
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.os.Environment
 import androidx.appcompat.app.AppCompatActivity
 import com.cnpeng.android2.R
-import com.cnpeng.android2.utils.TripleLibInitUtils
+import com.cnpeng.android2.utils.JsonParser
 import com.iflytek.cloud.*
+import com.tbruyelle.rxpermissions2.RxPermissions
+import kotlinx.android.synthetic.main.activity_sound_write.*
 import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.error
 import org.jetbrains.anko.toast
+import org.json.JSONException
+import org.json.JSONObject
 
 /**
  * CnPeng 2019/1/8 2:16 PM
@@ -18,29 +25,56 @@ import org.jetbrains.anko.toast
  */
 class SoundWriteActivity : AppCompatActivity() {
 
+    lateinit var mRecognizer: SpeechRecognizer
 
+    var mRecognizeResults: MutableMap<String, String> = mutableMapOf()
     private val mEngineType = SpeechConstant.TYPE_CLOUD
+    var mIsRecognizerSuccess = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sound_write)
 
         initRecognizer()
+        initClickListener()
     }
 
     private fun initRecognizer() {
+        mRecognizer = SpeechRecognizer.createRecognizer(this) { errorCode ->
+            AnkoLogger("听写初始化").error { "SpeechRecognizer init() code = $errorCode" }
 
-        var mRecognizer = SpeechRecognizer.createRecognizer(this, mInitListener);
-
-        toast(if (null == mRecognizer) {
-            "null"
-        } else {
-            mRecognizer.toString()
-        })
-
-        if (null == mRecognizer) {
-            return
+            if (errorCode != ErrorCode.SUCCESS) {
+                toast("初始化失败，错误码：$errorCode");
+            }
         }
+    }
+
+    private fun initClickListener() {
+        tv_recordStart.setOnClickListener {
+            checkPermission()
+        }
+    }
+
+    @SuppressLint("CheckResult")
+    private fun checkPermission() {
+        val rxPermissions = RxPermissions(this)
+        rxPermissions.request(Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.RECORD_AUDIO)
+                .subscribe { granted ->
+                    if (granted) {
+                        startRecognizer()
+                    } else {
+                        toast("权限不足，无法享用相关功能")
+                    }
+                }
+    }
+
+    private fun startRecognizer() {
+
+        mIsRecognizerSuccess = false
+
+        mRecognizer.setParameter(SpeechConstant.PARAMS, null)
 
         //设置返回结果格式，目前支持json,xml以及plain 三种格式，其中plain为纯听写文本内容
         mRecognizer.setParameter(SpeechConstant.RESULT_TYPE, "json");
@@ -59,19 +93,17 @@ class SoundWriteActivity : AppCompatActivity() {
         //设置标点符号,设置为"0"返回结果无标点,设置为"1"返回结果有标点
         mRecognizer.setParameter(SpeechConstant.ASR_PTT, "1");
 
-        //开始识别，并设置监听器
-        mRecognizer.startListening(mRecogListener);
-    }
+        // 设置音频保存路径，保存音频格式支持pcm、wav，设置路径为sd卡请注意WRITE_EXTERNAL_STORAGE权限
+        mRecognizer.setParameter(SpeechConstant.AUDIO_FORMAT, "wav")
+        mRecognizer.setParameter(SpeechConstant.ASR_AUDIO_PATH, Environment.getExternalStorageDirectory().toString() + "/msc/讯飞语音听写.wav")
 
-    /**
-     * 初始化监听器。
-     */
-    private val mInitListener = InitListener {
-        AnkoLogger("听写初始化").error { "SpeechRecognizer init() code = " + it };
-        if (it != ErrorCode.SUCCESS) {
-            toast("初始化失败，错误码：" + it);
+        //开始识别，并设置监听器——这样其实就是开始录音了.并且获取是否识别成功的响应
+        mIsRecognizerSuccess = ErrorCode.SUCCESS == mRecognizer.startListening(mRecogListener);
+
+        if (!mIsRecognizerSuccess) {
+            toast("语音识别失败，请重新打开页面再次尝试")
         }
-    };
+    }
 
     private val mRecogListener = object : RecognizerListener {
         /**
@@ -79,35 +111,62 @@ class SoundWriteActivity : AppCompatActivity() {
          * @param data - 录音数据，
          */
         override fun onVolumeChanged(p0: Int, p1: ByteArray?) {
-            toast("音量改变了")
             AnkoLogger("mRecogListener").error { "音量改变了" }
         }
 
-        override fun onResult(result: RecognizerResult?, p1: Boolean) {
-            toast("返回内容了：${result?.resultString}")
-            AnkoLogger("mRecogListener").error { "返回内容了：${result?.resultString}" }
+        override fun onResult(result: RecognizerResult?, isLast: Boolean) {
+            AnkoLogger("mRecogListener").error { "返回内容了——JSON 串：${result?.resultString}" }
 
+            if (null != result) {
+                //CnPeng 2019/1/8 5:06 PM 识别成功，就解析内容
+                val text = JsonParser.parseIatResult(result.getResultString())
+
+                var sn: String = ""
+                // 读取json结果中的sn字段
+                try {
+                    val resultJson = JSONObject(result.getResultString())
+                    sn = resultJson.optString("sn")
+                } catch (e: JSONException) {
+                    e.printStackTrace()
+                }
+
+                //CnPeng 2019/1/8 5:17 PM 对应的简写方式为：mRecognizeResults[sn] = text
+                mRecognizeResults[sn] = text
+
+                if (isLast) {
+                    //CnPeng 2019/1/8 5:25 PM 输入完成之后，显示在界面
+                    val resultBuffer = StringBuffer()
+                    for (key in mRecognizeResults.keys) {
+                        resultBuffer.append(mRecognizeResults[key])
+                    }
+
+                    tv_xfResult.setText(resultBuffer.toString())
+                }
+            }
         }
 
         override fun onBeginOfSpeech() {
-            toast("开始输入语音")
             AnkoLogger("mRecogListener").error { "开始输入语音" }
+            tv_xfResult.text = "正在输入..."
 
         }
 
         override fun onEvent(p0: Int, p1: Int, p2: Int, p3: Bundle?) {
-            toast("扩展事件？")
             AnkoLogger("mRecogListener").error { "扩展事件" }
         }
 
         override fun onEndOfSpeech() {
-            toast("语音输入结束")
             AnkoLogger("mRecogListener").error { "语音输入结束" }
+            tv_xfResult.text = "输入完成，正在识别..."
         }
 
-        override fun onError(p0: SpeechError?) {
-            toast("发生错误了")
-            AnkoLogger("mRecogListener").error { "发生错误了" }
+        override fun onError(error: SpeechError?) {
+            AnkoLogger("mRecogListener").error { "发生错误——错误码：${error?.errorCode}, 错误MESSAGE：${error?.message}，错误描述：${error?.getPlainDescription(true)}" }
+            tv_xfResult.text = "发生错误——错误码：${error?.errorCode},\n错误MESSAGE：${error?.message}，\n错误描述：${error?.getPlainDescription(true)}"
+
+            if (null != error) {
+                toast(error.getPlainDescription(true))
+            }
         }
     }
 }
